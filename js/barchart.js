1f8a3d395166e45b6b2f510b52f41f78b21cf651
VedanshiShah7 <56982656+VedanshiShah7@users.noreply.github.com>
2022-02-24 16:56:36 -0500

    csv charts

--- js/barchart.js
+++ js/barchart.js
@@ -123,10 +123,102 @@ svg1.selectAll(".bar") // take element svg and select all bar classes inside the
      .on("mousemove", mousemove1) // adds the event handler for when the mouse mouses
      .on("mouseleave", mouseleave1); // adds the event handler for when the mouse leaves
 
+/*
 
+  CSV data loaded - barchart
 
+*/
 
+// Add an svg to build within using dimensions set above
+const svg2 = d3 
+  .select("#csv-bar")
+  .append("svg")
+  .attr("width", width-margin.left-margin.right)
+  .attr("height", height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]);
 
+d3.csv("data/barchart.csv").then((data2) => {
+  
+  /*
 
+  Axes
 
+  */ 
 
+  // Find the max Y
+  let maxY2 = d3.max(data2, function(d) { return d.score; });
+
+  // sets up a scaling function for data value to pixel value
+  let yScale2 = d3.scaleLinear()
+              .domain([0,maxY2])
+              .range([height-margin.bottom,margin.top]);
+
+  // sets up a scaling function for data value to pixel value
+  let xScale2 = d3.scaleBand()
+              .domain(d3.range(data2.length)) 
+              .range([margin.left, width - margin.right])
+              .padding(0.1);
+
+  // Add y axis to the svg
+  svg2.append("g")
+    .attr("transform", `translate(${margin.left}, 0)`)
+    .call(d3.axisLeft(yScale2))
+    .attr("font-size", '20px');
+
+  // Add x axis to the svg
+  svg2.append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(xScale2)
+              .tickFormat(i => data2[i].name))
+      .attr("font-size", '20px');
+
+  /* 
+
+    Tooltip Set-up  
+
+  */
+
+  // Adds a tooltip with the information
+  let tooltip2 = d3.select("#csv-bar") 
+                  .append("div2") 
+                  .attr('id', "tooltip2") 
+                  .style("opacity", 0) 
+                  .attr("class", "tooltip");
+
+  // Mouseover event handler
+  let mouseover2 = function(event, d) {
+    tooltip2.html("Name: " + d.name + "<br> Score: " + d.score + "<br>")
+            .style("opacity", 1);
+  };
+
+  // Mouse moving event handler
+  let mousemove2 = function(event, d) {
+    tooltip2.style("left", (event.x)+"px") 
+            .style("top", (event.y + yTooltipOffset) +"px");
+  };
+
+  // Mouseout event handler
+  let mouseleave2 = function(event, d) { 
+    tooltip2.style("opacity", 0);
+  };
+
+  /* 
+
+    Bars 
+
+  */
+
+  // Adds multiple bar shapes to the data binded
+  svg2.selectAll(".bar") 
+    .data(data2) 
+    .enter() 
+    .append("rect") 
+      .attr("class", "bar") 
+      .attr("x", (d,i) => xScale2(i)) 
+      .attr("y", (d) => yScale2(d.score)) 
+      .attr("height", (d) => (height - margin.bottom) - yScale2(d.score))
+      .attr("width", xScale2.bandwidth()) 
+      .on("mouseover", mouseover2) 
+      .on("mousemove", mousemove2)
+      .on("mouseleave", mouseleave2)
+});
--- js/scatterplot.js
+++ js/scatterplot.js
@@ -8,10 +8,98 @@ Modified: 12/08/21
 
 // Build your scatterplot in this file 
 
+/*
 
+  CSV data loaded - scatterplot
 
+*/
 
+// Add an svg to build within using dimensions set above
+const svg3 = d3 
+  .select("#csv-scatter")
+  .append("svg")
+  .attr("width", width-margin.left-margin.right)
+  .attr("height", height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]);
 
+d3.csv("data/scatter.csv").then((data3) => {
+  
+    /*
 
+    Axes
 
+    */ 
 
+    // find max x
+    let maxX3 = d3.max(data3, (d) => { return d.day; }); 
+
+    // find max Y 
+    let maxY3 = d3.max(data3, (d) => { return d.score; }); 
+
+    // sets up a scaling function for data value to pixel value
+    let xScale3 = d3.scaleLinear()
+                    .domain([0, maxX3])
+                    .range([margin.left, width - margin.right]); 
+
+    // sets up a scaling function for the data value to pixel value on the y scale    
+    let yScale3 = d3.scaleLinear()
+                .domain([0, maxY3])
+                .range([height - margin.bottom, margin.top]); 
+
+    // Add y axis to the svg
+    svg3.append("g")
+        .attr("transform", `translate(${margin.left}, 0)`) 
+        .call(d3.axisLeft(yScale3))
+        .attr("font-size", '20px');
+    
+
+    // Add x axis to the svg
+    svg3.append("g")
+        .attr("transform", `translate(0,${height - margin.bottom})`) 
+        .call(d3.axisBottom(xScale3))
+            .attr("font-size", '20px');
+
+    /* 
+
+    Tooltip Set-up  
+
+    */
+
+    // Adds a tooltip with the information
+    let tooltip3 = d3.select("#csv-scatter") 
+                    .append("div3") 
+                    .attr('id', "tooltip3") 
+                    .style("opacity", 0) 
+                    .attr("class", "tooltip");
+
+    // Mouseover event handler
+    let mouseover3 = function(event, d) {
+    tooltip3.html("Name: " + d.day + "<br> Score: " + d.score + "<br>")
+            .style("opacity", 1);
+    };
+
+    // Mouse moving event handler
+    let mousemove3 = function(event, d) {
+    tooltip3.style("left", (event.x)+"px") 
+            .style("top", (event.y + yTooltipOffset) +"px");
+    };
+
+    // Mouseout event handler
+    let mouseleave3 = function(event, d) { 
+    tooltip3.style("opacity", 0);
+    };
+
+    svg3.selectAll(".circle") 
+        .data(data3)
+        .enter()  
+        .append("circle")
+            .attr("class", "circle")
+            .attr("cx", (d,i) => xScale3(d.day))
+            .attr("cy", (d) => yScale3(d.score))
+            .attr("r", 10)
+            .attr("height", (d) => (height - margin.bottom) - yScale3(d.score))
+            .attr("width", (d) => (height - margin.bottom) - xScale3(d.day))
+            .on("mouseover", mouseover3) 
+            .on("mousemove", mousemove3)
+            .on("mouseleave", mouseleave3);
+    });
